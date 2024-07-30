f41b267a1ba4f31368b2dc62116e589cc4b06966
strongman1012 <alexander.schvetc.2000@gmail.com>
2024-07-30 19:34:31 +0300

    backend url update-3

--- src/utills/config.ts
+++ src/utills/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const REACT_APP_SERVER_URL = "https://linkbudgettesting.azurewebsites.net";
-export const LOCAL_SERVER_URL = "https://7cbe-89-187-161-220.ngrok-free.app/api/v0";
+export const LOCAL_SERVER_URL = "https://react-userroles-backend.onrender.com/api/v0";
 export const API_VERSION = "v0";
 
 export const apiClient = axios.create({
