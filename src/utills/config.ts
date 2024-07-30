bdba06238cdb74318a31696221dd173eb7974676
strongman1012 <alexander.schvetc.2000@gmail.com>
2024-07-30 15:54:45 +0300

    backend url replace

--- src/utills/config.ts
+++ src/utills/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const REACT_APP_SERVER_URL = "https://linkbudgettesting.azurewebsites.net";
-export const LOCAL_SERVER_URL = "http://localhost:5000/api/v0";
+export const LOCAL_SERVER_URL = "https://7cbe-89-187-161-220.ngrok-free.app";
 export const API_VERSION = "v0";
 
 export const apiClient = axios.create({
